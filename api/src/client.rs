d378111e821c91598d4d55c05aa6beea7ce53d40
aaron.tillekeratne <aaron.tillekeratne@jahan.ai>
2024-07-22 00:44:14 +0000

    added client

---
+++ api/src/client.rs
@@ -0,0 +1,18 @@
+use recovery_tracker::tracker_client::TrackerClient;
+use recovery_tracker::{EmptyRequest, HealthzResponse};
+
+pub  mod recovery_tracker{ 
+    tonic::include_proto!("recovery_tracker");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> { 
+    let mut client = TrackerClient::connect("http://0.0.0.0:5051").await?;
+    
+    let request = tonic::Request::new(EmptyRequest{});
+
+    let response = client.healthz(request).await?;
+
+    println!("RESPONSE={:?}", response);
+    Ok(())
+}
